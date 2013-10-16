22d4d1a616e66d140a80156c4ef512ad7d44c41c
xupingmao <xupingmao@gmail.com>
2013-10-16 23:55:38 +0800

    home导航，侧边栏

--- WebOrdering/WebOrdering/home_view.py
+++ WebOrdering/WebOrdering/home_view.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.template import Template,Context
 
 def home(request):
     text=open('WebOrdering/home.html').read()
-    return HttpResponse(text)
+    foods=[('img/food1.jpg','apple'),('img/food1.jpg','google'),('img/food1.jpg','microsoft'),]
+    return HttpResponse(Template(text).render(Context({'foods':foods})))
 
 def css_resource(request,fname):
     text=open('WebOrdering/'+fname+'.css').read()
--- WebOrdering/WebOrdering/js/home.js
+++ WebOrdering/WebOrdering/js/home.js
@@ -3,4 +3,17 @@ $(function(){
 		$('#menu li').removeClass();
 		$(this).addClass('active');
 	})
+	
+	$('#sidebar li').click(function(){
+		$('#sidebar li').removeClass();
+		$(this).addClass('sidebar_active')
+	})
+	
+	$('.food_pic img').hover(function(){
+		$(this).removeClass();
+		$(this).addClass('pic_show')
+	}, function(){
+		$(this).removeClass();
+		$(this).addClass('pic_dark')
+	})
 })
