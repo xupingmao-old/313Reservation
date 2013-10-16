db190d9ef26a486b3c4c52fd6adaff15f7eae46b
xupingmao <xupingmao@gmail.com>
2013-10-16 17:58:42 +0800

    home

--- WebOrdering/WebOrdering/home_view.py
+++ WebOrdering/WebOrdering/home_view.py
@@ -3,3 +3,23 @@ from django.http import HttpResponse
 def home(request):
     text=open('WebOrdering/home.html').read()
     return HttpResponse(text)
+
+def css_resource(request,fname):
+    text=open('WebOrdering/'+fname+'.css').read()
+    return HttpResponse(text)
+
+def jpg_resource(request,fname):
+    text=open('WebOrdering/'+fname+'.jpg','rb').read()
+    return HttpResponse(text)
+
+def gif_resource(request,fname):
+    text=open('WebOrdering/'+fname+'.gif','rb').read()
+    return HttpResponse(text)
+
+def png_resource(request,fname):
+    text=open('WebOrdering/'+fname+'.png','rb').read()
+    return HttpResponse(text)
+
+def js_resource(request,fname):
+    text=open('WebOrdering/'+fname+'.js','rb').read()
+    return HttpResponse(text)
---
+++ WebOrdering/WebOrdering/js/home.js
@@ -0,0 +1,6 @@
+$(function(){
+	$('#menu li').click(function(){
+		$('#menu li').removeClass();
+		$(this).addClass('active');
+	})
+})
