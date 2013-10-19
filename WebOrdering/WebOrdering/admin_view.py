14a2c7a2611bc57f03eabdf76d001663c5a6655b
xupingmao <xupingmao@gmail.com>
2013-10-19 22:12:06 +0800

    upload file

--- WebOrdering/WebOrdering/admin_view.py
+++ WebOrdering/WebOrdering/admin_view.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template import Template,Context
+from django.shortcuts import render_to_response
 import sqlite3
 import re
+import os
+import time
+
 
 def admin(request):
     text=open('WebOrdering/admin_login.html').read()
@@ -10,12 +14,13 @@ def admin(request):
 def admin_login(request):
     text=open('WebOrdering/admin_login.html').read()
     username,password='',''
-    if 'username' in request.GET:
-        username=request.GET['username']
-    if 'password' in request.GET:
-        password=request.GET['password']
+    if 'username' in request.POST:
+        username=request.POST['username']
+    if 'password' in request.POST:
+        password=request.POST['password']
     if is_admin(username,password):
         return HttpResponseRedirect('/admin/'+username)
+    text=Template(text).render(Context({}))
     return HttpResponse(text)
 
 def is_admin(account,md5_password):
@@ -30,5 +35,42 @@ def is_admin(account,md5_password):
     else:return False
 
 def admin_manage(request,username):
+    base='WebOrdering/img/'
+    if request.FILES.has_key('pic'):
+        pic=request.FILES['pic']
+        extension=get_extension(pic)
+        if extension:
+            print pic,'uploaded'
+            fname=base+str(time.time())+extension
+            fp=open(fname,'wb')
+            fp.write(pic.read())
+            fp.close()
+            return HttpResponseRedirect('/admin/'+username)
     text=open('WebOrdering/admin_manage.html').read()
     return HttpResponse(Template(text).render(Context({'admin_name':username})))
+
+
+def get_extension(name):
+    name=str(name)
+    if name.endswith(('.jpg','.JPG')):
+        return '.jpg'
+    elif name.endswith(('.png','.PNG')):
+        return '.png'
+    elif name.endswith(('.gif','.GIF')):
+        return '.gif'
+    else:
+        return None
+
+def add_to_db(foodname,foodimage,foodprice,category,introduce):
+    db=sqlite3.connect('WebOrdering/menu.db')
+    cur=db.cursor()
+    cur.execute("insert in menu values(%d,'%s','%s',%f,'%s','%s')",
+        int(time.time()),foodname,foodimage,foodprice,category,introduce)
+    cur.commit()
+    db.close()
+
+def get_food_list():
+    db=sqlite3.connect('WebOrdering/menu.db')
+    cur=db.cursor()
+    for item in cur.execute('select * from menu'):
+        yield item
