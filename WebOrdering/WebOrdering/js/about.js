93e2279dde82ebc9244365c22a7990f9f01858ce
lixinwei1230 <lixinwei1230@foxmail.com>
2013-10-22 09:04:09 +0800

    改到蛋碎的about.html

--- WebOrdering/WebOrdering/js/about.js
+++ WebOrdering/WebOrdering/js/about.js
@@ -20,7 +20,7 @@ $(function(){
 })
 
 function showSection (id) {
-	var sections = document.getElementsByTagNames("sections");
+	var sections = document.getElementsByTagName("sections");
 	for (var i=0;i<sections.length;i++){
 		if (sections[i].getAttribute("id") != id){
 		sections[i].style.dispaly = "none";
@@ -29,3 +29,38 @@ function showSection (id) {
 	    }
     }
 }
+
+function prepareInternalnav () {
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById) return false;
+	var articles = document.getElementsByTagName("article");
+	if (articles.length == 0) return false;
+	var navs = articles[0].getElementsByTagNames("nav");
+	if (navs.length == 0) return false;
+	var nav = navs[0];
+	var links = nav.getElementsByTagName("a");
+	for (var i=0;i<links.length;i++){
+		var sectionId = links[i].getAttribute("href").split("#")[1];
+	    if (!document.getElementById(sectionId)) continue;
+	    document.getElementById(sectionId).style.dispaly = "none";
+	    links[i].destination = sectionId;
+	    links[i].onclick = function(){
+	    	showSection(this.destination);
+	    	return false;
+	    }
+	}    
+}
+
+function addLoadEvent(func){
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function'){
+		window.onload = func;
+	}else{
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
+}
+
+addLoadEvent(prepareInternalnav);
