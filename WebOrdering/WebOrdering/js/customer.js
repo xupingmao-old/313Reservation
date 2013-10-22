01afd8f0111964843d4163188bbb639f599f0116
lixinwei1230 <lixinwei1230@foxmail.com>
2013-10-22 13:34:09 +0800

    更改customer.html

--- WebOrdering/WebOrdering/js/customer.js
+++ WebOrdering/WebOrdering/js/customer.js
@@ -84,6 +84,22 @@ function phoneShow_old_2()
 	d.checked = false;
 }
 
-function home () {
-	window.location.href="/";
-}
+$(function(){
+	$('#menu li').click(function(){
+		$('#menu li').removeClass();
+		$(this).addClass('active');
+	})
+	
+	$('.login_form_input').click(function(){
+		$('#menu li').removeClass();
+		$(this).addClass('active');
+	})
+
+	$('#about').click(function(){
+		window.location.href='../about';
+	})
+	$('#home').click(function(){
+		window.location.href='/';
+	})
+})
+
